b5ae469af61dccd339237e74aa186ac0d58e55a2
Isabella Tobias <74330693+tobiasisabella@users.noreply.github.com>
2020-11-12 08:52:27 -0300

    Update run.py

--- run.py
+++ run.py
@@ -16,43 +16,43 @@ Created on Thu Oct 22 17:50:23 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Open asset file
+#Open active file
 #The first column contains the date and the second contains the values
 #Selects the first column
 #Recognizes column data as date (DD / MM / YY)
 #Selects the second column
 
 data = pd.read_csv("01.csv")
-data_ativos = data.iloc[:,0] 
-data_ativos = [pd.to_datetime(d) for d in data_ativos]
-venda_ativos = data.iloc[:,1]
+data_active = data.iloc[:,0] 
+data_active = [pd.to_datetime(d) for d in data_active]
+sell_active = data.iloc[:,1]
 
 #Open the file of canceled values
 #Selects the first column
 #Recognizes column data as date (DD / MM / YY)
 #Selects the second column
 data = pd.read_csv("02.csv")
-data_cancelados = data.iloc[:,0] 
-data_cancelados = [pd.to_datetime(d) for d in data_cancelados]
-venda_cancelados = data.iloc[:,1]
+data_canceled = data.iloc[:,0] 
+data_canceled = [pd.to_datetime(d) for d in data_canceled]
+sell_canceled = data.iloc[:,1]
 
 
 #Figure size
 #Plot the active data
 #Plot the canceled data
 plt.figure(figsize = (15,10))
-plt.scatter(data_ativos,venda_ativos, color='b', label='Ativos')
-plt.scatter(data_cancelados,venda_cancelados, color='r', label='Cancelados')
+plt.scatter(data_active,sell_active, color='b', label='Active')
+plt.scatter(data_canceled,sell_canceled, color='r', label='Canceled')
 
 #Name the x axis
 #Name the y-axis
 #Position of the subtitles
 #Place the title
 #Place grid
-plt.xlabel('Data')
-plt.ylabel('Número de Vendas')
+plt.xlabel('Date')
+plt.ylabel('Number of Sells')
 plt.legend(loc='best')
-plt.title("Ciclo 1")
+plt.title("Cicle")
 plt.grid(True)
 
 #Displays the graph
